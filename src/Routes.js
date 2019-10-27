ba5fcb70ee023110d25fa3a3b1df1bf99eb4bc9e
unknown <ivan.mitkov@gmail.com>
2019-10-27 12:48:24 +0200

    fixed redirect from login

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Switch, Route  } from "react-router-dom";
+import { Switch, Route,Redirect  } from "react-router-dom";
 
 import Characters from "./pages/Characters"
 import Episodes from "./pages/Episodes"
 import EpisodeDetail from "./pages/Episode.detail"
 import CharacterDetail from "./pages/Characters.details"
 import Starship from "./pages/Starship"
-// import Login from "./pages/Login"
+import Login from "./pages/Login"
 
 const Routes = () => {
   return (
@@ -15,7 +15,7 @@ const Routes = () => {
       <Route  component={EpisodeDetail} path="/episodes/:episodeId" />
       <Route  component={CharacterDetail} path="/characters/:characterId" />
       <Route  component={Starship} path="/starships/:starshipId" />
-      {/* <Route exact component={Login} path="/login" /> */}
+      <Route exact component={()=><Redirect to ='/'/>} path="/login" />
       <Route  exact component={Episodes} path="/episodes" />
       <Route  component={Episodes} path="/" />
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,Redirect  } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 
