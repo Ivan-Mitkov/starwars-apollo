cb92f4ee3230e98e226dab9732e31b71e817673d
unknown <ivan.mitkov@gmail.com>
2019-10-30 08:30:31 +0200

    episode detail with apollo

--- src/pages/Episode.detail.js
+++ src/pages/Episode.detail.js
@@ -2,15 +2,38 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { withTheme } from "styled-components";
 import { withRouter } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import Loading from "../components/Loading";
 
-import { movies } from "../data/movies2";
+// import { movies } from "../data/movies2";
 import CartEpisodeDetail from "../components/EpisodeDetail/CartEpisodeDetail";
 import TextEpisodeDetail from "../components/EpisodeDetail/TextEpisodeDetail";
 import PeopleList from "../containers/PersonsList";
+
+const ALL_EPISODES = gql`
+  query allEpisodes($first: Int!) {
+    allEpisodes(first: $first) {
+      edges {
+        node {
+          title
+          director
+          episodeId
+          releaseDate
+          openingCrawl
+          image
+        }
+      }
+    }
+  }
+`;
+
 const EpisodesPage = styled.div`
   height: 100vh;
 `;
 const EpisodeDetail = props => {
+  const [movie, setMovie] = useState(null);
+  const [title, setTitle] = useState(null);
   const roman = {
     "1": "I",
     "2": "II",
@@ -20,25 +43,26 @@ const EpisodeDetail = props => {
     "6": "VI",
     "7": "VII"
   };
-  const [movie, setMovie] = useState(null);
-  const [title, setTitle] = useState(null);
+  // let ep = props.match.params.episodeId;
+  ///from Apolo
+
   useEffect(() => {
     const fetchData = async () => {
       let ep = props.match.params.episodeId;
-      const result = await movies;
+      const result = await data;
+      // console.log(result)
       if (result) {
-        const episode = result.data.allEpisodes.edges.find(x => {
+        const episode = result.allEpisodes.edges.find(x => {
           // console.log(x.node.episodeId);
           return x.node.episodeId.toString() === ep;
         });
         if (episode) {
           setMovie(episode.node);
           setTitle(`Star Wars: Episode ${roman[ep]}`);
-        }
-        else{
+        } else {
           const episode = result.data.allEpisodes.edges.find(x => {
             // console.log(x.node.episodeId);
-            return x.node.episodeId.toString() === '1';
+            return x.node.episodeId.toString() === "1";
           });
           setMovie(episode.node);
           setTitle(`Star Wars: Episode ${roman[1]}`);
@@ -47,6 +71,20 @@ const EpisodeDetail = props => {
     };
     fetchData();
   }, [props, roman]);
+  ///from Apolo
+  const { loading, error, data } = useQuery(ALL_EPISODES, {
+    variables: { first: 7 }
+  });
+  if (loading) return <Loading />;
+  if (error) {
+    console.log(error);
+    return <p>Error on getting all movies</p>;
+  }
+
+  const {
+    allEpisodes: { edges, cursor }
+  } = data;
+
   return (
     <EpisodesPage>
       {/* {console.log(movie)} */}
